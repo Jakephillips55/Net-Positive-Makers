d8435ffed90c111640f8b1a7ca81a623c8d5bbb7
David Papamichael <davidpapamichael@gmail.com>
2020-03-03 10:48:57 +0000

    class for vectors, ball and rectangle made

--- pongGame/pong.js
+++ pongGame/pong.js
@@ -1,7 +1,40 @@
+
+class Vector
+{
+  constructor(x = 0, y = 0)
+  {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class Rectangle
+{
+  constructor(w, h)
+  {
+    this.position = new Vector;
+    this.size = new Vector(w, h)
+  }
+}
+
+class Ball extends Rectangle
+{
+  constructor()
+  {
+    super(10, 10)
+    this.velocity = new Vector;
+  }
+}
+
+
+
 // creates the canvas
 const canvas = document.getElementById('pong');
 // makes the object 2d and includes methods and properties for drawing
 const context = canvas.getContext('2d');
+//creates new ball
+const ball = new Ball;
+
 // fills the canvas with black (specified width and height in html)
 context.fillStyle = '#000';
 context.fillRect(0, 0, canvas.width, canvas.height);
