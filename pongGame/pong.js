186f4540dc69d4048fa9b6b83dd8789eb2e0983d
David Papamichael <davidpapamichael@gmail.com>
2020-03-03 12:33:15 +0000

    callback wrapped in Pong class, update function to include wall bounce that returns negative velocity

--- pongGame/pong.js
+++ pongGame/pong.js
@@ -16,6 +16,22 @@ class Rectangle
     this.position = new Vector;
     this.size = new Vector(w, h)
   }
+  get left()
+  {
+    return this.position.x - this.size.x / 2;
+  }
+  get right()
+  {
+    return this.position.x + this.size.x / 2;
+  }
+  get top()
+  {
+    return this.position.y - this.size.y / 2;
+  }
+  get bottom()
+  {
+    return this.position.y + this.size.y / 2;
+  }
 }
 
 class Ball extends Rectangle
@@ -27,34 +43,50 @@ class Ball extends Rectangle
   }
 }
 
+class Pong
+{
+  constructor(canvas)
+  {
+    this._canvas = canvas;
+    this._context = canvas.getContext('2d');
+
+    this.ball = new Ball;
+    this.ball.position.x = 100;
+    this.ball.position.y = 50;
+
+    this.ball.velocity.x = 100;
+    this.ball.velocity.y = 100;
+
+   let lastTime;
+   const callback = (milliseconds) => {
+      if (lastTime) {
+        this.update((milliseconds - lastTime) / 1000);
+      }
+      lastTime = milliseconds;
+      requestAnimationFrame(callback);
+    }
+    callback();
+  }
+  update(deltatime) {
+    this.ball.position.x += this.ball.velocity.x * deltatime;
+    this.ball.position.y += this.ball.velocity.y * deltatime;
+  
+    if (this.ball.left < 0 || this.ball.right > this._canvas.width) {
+      this.ball.velocity.x = -this.ball.velocity.x
+    }
+  
+    if (this.ball.top < 0 || this.ball.bottom > this._canvas.height) {
+      this.ball.velocity.y = -this.ball.velocity.y
+    }
+  
+    this._context.fillStyle = '#000';
+    this._context.fillRect(0, 0, this._canvas.width, this._canvas.height);
+  
+    this._context.fillStyle = '#fff';
+    this._context.fillRect(this.ball.position.x, this.ball.position.y, this.ball.size.x, this.ball.size.y);
+  
+  }
+}
 
 const canvas = document.getElementById('pong');
-const context = canvas.getContext('2d');
-
-const ball = new Ball;
-ball.position.x = 100;
-ball.position.y = 100;
-ball.velocity.Ballx = 100;
-ball.velocity.y = 100;
-
-var  lastTime;
-function callback(milliseconds) {
-  if (lastTime) {
-    update((milliseconds - lastTime) / 1000);
-  }
-  lastTime = milliseconds;
-  requestAnimationFrame(callback);
-}
-function update(deltatime) {
-  ball.position.x += ball.velocity.x * deltatime;
-  ball.position.y += ball.velocity.y * deltatime;
-
-  context.fillStyle = '#000';
-  context.fillRect(0, 0, canvas.width, canvas.height);
-
-  context.fillStyle = '#fff';
-  context.fillRect(ball.position.x, ball.position.y, ball.size.x, ball.size.y);
-
-}
-
-callback()
+const pong = new Pong(canvas);
