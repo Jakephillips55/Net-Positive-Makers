13468c0a02f60b170b08e8cd76f870e3f473f012
Jakephillips55 <Jakep629@gmail.com>
2020-03-04 15:33:10 +0000

    commiting framework for adding / tracking scores in the pong games view - will help david with logic now

--- net_positive/pong/views.py
+++ net_positive/pong/views.py
@@ -5,7 +5,7 @@ from pong.models import SimpleBot
 
 def home(request):
     return HttpResponse(
-'<!DOCTYPE html><html lang="en"><head><title>Pong</title><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"></head><body><h1>Pong</h1><canvas id="pong" width="600" height="400"></canvas><script src="static/pong.js"></script></body>')
+'<!DOCTYPE html><html lang="en"><head><title>Pong</title><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"></head><body onload="startBall()"><h1 style="osition:absolute;left:45%;top:10px;font-size:70px;"<h1>Pong</h1><h1 style=”position:absolute;left:30px;top:80px;”>Player: <span id="score1">0</span></h1><h1 style=”position:absolute;right:30px;top:80px;”>A.I: <span id="score2">0</span></h1><hr style="margin-top:150px;"><canvas id="pong" width="600" height="400"></canvas><script src="static/pong.js"></script></body>')
 
 def bot(request):
 
