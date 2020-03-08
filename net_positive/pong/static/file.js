8ce8e9d054247d7388892ec907f015ba71bb040d
Jakephillips55 <Jakep629@gmail.com>
2020-03-08 23:32:20 +0000

    have 200 html code so static js files are being found js just refuses to write the file will look into this in the morning MIME type error shouldnt be too rough.

---
+++ net_positive/pong/static/file.js
@@ -0,0 +1,6 @@
+document.write("<script type ='text/javascript' src='pong.js'><\/script>");
+document.write("<script type ='text/javascript' src='Ball.js'><\/script>");
+document.write("<script type ='text/javascript' src='Game.js'><\/script>");
+document.write("<script type ='text/javascript' src='Player.js'><\/script>");
+document.write("<script type ='text/javascript' src='Rectangle.js'><\/script>"); 
+document.write("<script type ='text/javascript' src='Vector.js'><\/script>"); 
