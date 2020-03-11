1148119851414063821f187128a0c9d0b75b3586
Jakephillips55 <Jakep629@gmail.com>
2020-03-11 07:29:18 +0000

    got a template image to loado load from our own files issue was that django dosent naturally serve media files, why ? who knows

--- net_positive/net_positive/settings.py
+++ net_positive/net_positive/settings.py
@@ -124,11 +124,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(os.path.abspath(
-    os.path.join(BASE_DIR, 'net_positive', 'static')), '')
+    os.path.join(BASE_DIR, 'net_positive', 'static',)), '')
 
 STATIC_URL = '/static/'
 
 django_heroku.settings(locals())
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
 
 
 # Channels
