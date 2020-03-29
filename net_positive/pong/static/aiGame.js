7e175ae83ab0952317360d8b55c847e7bb335084
ratcliffetj <ratcliffetj89@gmail.com>
2020-03-29 15:31:06 +0100

    refactored ball paddle collide function

--- net_positive/pong/static/aiGame.js
+++ net_positive/pong/static/aiGame.js
@@ -73,20 +73,24 @@ class Ball extends Rectangle {
     }
   }
 
-  collidePaddle(player, canvasWidth) {
-    if (player.left <= this.right && player.right >= this.left && player.top <= this.bottom && player.bottom >= this.top) {
-      //move ball back to the face of the paddle
-      if (this.position.x > canvasWidth/2) {
-        this.position.x = canvasWidth - player.paddleOffsetStart - player.size.x/2;
-      }
-      else {
-        this.position.x = player.paddleOffsetStart + player.size.x/2;
-      }
-      //reverse x velocity, add some randomness on top of existing y velocity, speed up by 5%
-      this.velocity.x = -this.velocity.x;
-      this.velocity.y += this.velocity.y * (Math.random() - 0.5);
-      this.velocity.length *= 1.05; 
+  collideLeftPaddle(player) {
+    if (player.right >= this.left && player.top <= this.bottom && player.bottom >= this.top) {
+      this.position.x = player.paddleOffsetStart + player.size.x/2;
+      this.paddleBounce();
     }
+  }
+
+  collideRightPaddle(player, canvasWidth) {
+    if (player.left <= this.right && player.top <= this.bottom && player.bottom >= this.top) {
+      this.position.x = canvasWidth - player.paddleOffsetStart - player.size.x/2;
+      this.paddleBounce();
+    }
+  }
+
+  paddleBounce() {
+    this.velocity.x = -this.velocity.x;
+    this.velocity.y += this.velocity.y * (Math.random() - 0.5);
+    this.velocity.length *= 1.05; 
   }
 }
 
@@ -125,7 +129,7 @@ class Player extends Rectangle {
     }
   }
 
-  moveDown(moveSpeed) {
+  moveDown(moveSpeed, canvasHeight) {
     if (this.isMoveInCourtBottom(moveSpeed, canvasHeight)) {
       this.position.y += this.botSpeed;
     }
@@ -170,30 +174,21 @@ class BotSocket extends WebSocket {
 }
 
 class ImageProcessor {
+  constructor() {
+    this.regex80 = /00000000000000000000000000000000000000000000000000000000000000000000000000000000/gi
+    this.regex40 = /0000000000000000000000000000000000000000/gi
+    this.regex20 = /00000000000000000000/gi
+    this.regex10 = /0000000000/gi
+    this.regex4 = /1111/gi
+    this.regexW = /wwwwwwwwwwwwwwwwwwww/gi
+  }
+
   retrievePixelData(context) {
     var image = context.getImageData(0, 0, 320, 320);
     var imageArray = Array.from(image.data);
     imageArray = this.rgbaToBinary(imageArray);
     var imageString = imageArray.join('');
     imageString = this.compressString(imageString);
-    return imageString;
-  }
-
-  compressString(imageString) {
-    //first round of compression
-    var regex80 = /00000000000000000000000000000000000000000000000000000000000000000000000000000000/gi
-    var regex40 = /0000000000000000000000000000000000000000/gi
-    var regex20 = /00000000000000000000/gi
-    var regex10 = /0000000000/gi
-    var regex4 = /1111/gi
-    imageString = imageString.replace(regex80, 'w');
-    imageString = imageString.replace(regex40, 'x');
-    imageString = imageString.replace(regex20, 'y');
-    imageString = imageString.replace(regex10, 'z');
-    imageString = imageString.replace(regex4, 'a');
-    // second round of compression
-    var regexW = /wwwwwwwwwwwwwwwwwwww/gi
-    imageString = imageString.replace(regexW, 'v');
     return imageString;
   }
 
@@ -206,6 +201,18 @@ class ImageProcessor {
       imageArray[i] < 127 ? imageArray[i] = 0 : imageArray[i] = 1;
     }
     return imageArray;
+  }
+
+  compressString(imageString) {
+    //first round of compression
+    imageString = imageString.replace(this.regex80, 'w');
+    imageString = imageString.replace(this.regex40, 'x');
+    imageString = imageString.replace(this.regex20, 'y');
+    imageString = imageString.replace(this.regex10, 'z');
+    imageString = imageString.replace(this.regex4, 'a');
+    // second round of compression
+    imageString = imageString.replace(this.regexW, 'v');
+    return imageString;
   }
 
 }
@@ -353,7 +360,8 @@ class Pong {
 
     this.updateScore();
     this.ball.collideSides(this._canvas.height);
-    this.players.forEach(player => this.ball.collidePaddle(player, this._canvas.width));
+    this.ball.collideLeftPaddle(this.players[0]);
+    this.ball.collideRightPaddle(this.players[1], this._canvas.width);
   }
 
   updateScore() {
