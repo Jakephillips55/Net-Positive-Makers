908590d1430b8ca4e117dff569acc5c64b76036d
ratcliffetj <ratcliffetj89@gmail.com>
2020-03-30 11:30:07 +0100

    Split python models into separate model files

---
+++ net_positive/pong/models/faultyBot.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class FaultyBot(models.Model):
+    @classmethod
+    def faulty_bot_ws(request, bally, paddley):
+      if str(bally) <= str(paddley):
+        return True
+      else:
+        return False
---
+++ net_positive/pong/models/nonPerfectBot.py
@@ -0,0 +1,18 @@
+from django.db import models
+import numpy as np
+
+class NonPerfectBot(models.Model):
+    @classmethod
+    def non_perfect_bot_ws(request, bally, paddley):
+      sample = np.random.uniform()
+      if int(bally) <= int(paddley):
+        if sample >= 0.1:
+          move_up = True
+        else:
+          move_up = False 
+      else:
+        if sample >= 0.1:
+          move_up = False 
+        else:
+          move_up = True
+      return move_up
